77d637500f6e3b4bf46e42a714b447a0499d2cf9
Alex Hunter <alexander.hunter.80@gmail.com>
2023-11-04 09:28:29 -0700

    working on slash command options

--- bot.py
+++ bot.py
@@ -1,40 +1,63 @@
-# Import various libraries.
+# Logging.
+import logging
+import logging.config
+logging.config.fileConfig("logging.conf")
+log = logging.getLogger("bot")
+log.debug("Logger initialized")
+
+
+
+# Imports.
 import os, discord, urllib.request, asyncio
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
 
-# Set the token.
+
+
+# Token.
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-print(DISCORD_TOKEN)
 
-# Define whatever this is.
+
+
+# Intents.
 intents = discord.Intents.default()
 intents.message_content = True
 intents.reactions = True
-print(intents)
-client = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+
 
 # Message respondings.
-@client.event
+@bot.event
 async def on_message(message):
     print("message was: " + message.content)
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     if message.content == '!radar':
         urllib.request.urlretrieve('https://radar.weather.gov/ridge/standard/CONUS_loop.gif', "rdr.gif")
         await message.channel.send(file=discord.File('rdr.gif'))
 
-#Commands
-@client.tree.command(name="radar", description="Retrieves the live doppler radar for a station, if none given, gets CONUS.")
+
+
+# Commands.
+@bot.tree.command(name="radar", description="Retrieves the live doppler radar for a station, if none given, gets CONUS.")
 async def _space(ctx: discord.interactions.Interaction):
 	await ctx.response.send_message(file=discord.File('rdr.gif'))
 
-@client.event
-async def on_ready():
-    client.change_presence(status=discord.Status.online)
-    await client.tree.sync()
-    print(f'We have logged in as {client.user}')
 
-client.run(DISCORD_TOKEN)
+@bot.tree.command(name="ready", description="Call a ready check")
+async def readycheck(interaction: discord.interactions.Interaction, target: int, mention: str, unique: bool):
+    log.debug(f'Received readycheck: %s, %s, %s', target, mention, unique)
+    await interaction.channel.send('readycheck received!')
+
+
+# Bootstrap.
+@bot.event
+async def on_ready():
+    bot.change_presence(status=discord.Status.online)
+    await bot.tree.sync()
+    print(f'We have logged in as {bot.user}')
+
+bot.run(DISCORD_TOKEN)
 print('Finished')
